06b926ee85e3b4e779d2d1ff43caa8db7d0ac051
FahreziP <fahrezipurwadianitra@gmail.com>
2025-06-26 10:20:33 +0700

    membuat void insert di dalam class binarytree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -22,6 +22,28 @@ class BinaryTree{
         root = nullptr; // initialize root to null
     }
 
+    void insert(){
+        int x;
+        cout << "Masukkan Nilai: ";
+        cin >> x;
+        
+        // step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+        // step 2: Assign the value to the data field of new node
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of new node point as null
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        // Step  4: Locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
+
+
+    }
 
 };
 
